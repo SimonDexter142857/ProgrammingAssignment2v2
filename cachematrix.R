d5a0ef0ad31299354ff8640a32d216040c818709
SimonDexter142857 <simondex@yahoo.com>
2014-09-19 07:07:39 -0400

    09/19/14: committing first version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,79 @@
-## Put comments here that give an overall description of what your
-## functions do
+#################################################
+# Author:   Simon Dexter, simondex@yahoo.com
+# Date:     09/19/2014
+#
+# Description: 
+#           Cache matrix programming assignment
+#################################################
 
-## Write a short comment describing this function
+
+#---------------------------------------------
+# Returns a special matrix object which maintains 
+# its inverse in cache
+#---------------------------------------------
+
 
 makeCacheMatrix <- function(x = matrix()) {
+	
+	# storage for cache
+	inv = NULL;
+      # actual matrix
+	m = NULL;
 
+      if (!is.null(x) ) m = x;
+
+	# setting matrix data and initializing
+	# inverse matrix to null
+	set = function(y) {
+		m <<- y; 
+		inv <<- NULL;
+	}
+
+	
+	# returning matrix data
+	get = function () m;
+	
+	# setting the inverse if not computed yet
+	setInverse = function(inverse) inv <<- inverse;
+	
+
+	# getting the inverse 
+	getInverse = function() inv; 
+	
+	# creating and returning the 
+	# resultant object
+	list (set = set, get = get, setInverse = setInverse, getInverse = getInverse);
+	
 }
 
 
-## Write a short comment describing this function
+
+#---------------------------------------------
+# Retrieves inverse of matrix contained 
+# in an object returned by makeCacheMatrix
+# function, otherwise computes the inverse
+# and stores in the object
+#---------------------------------------------
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      # retrieving the inverse 
+      # from the object
+      inv = x$getInverse();
+	
+      # the inverse is already computed 
+      if (!is.null (inv)) {
+           message ("getting cached data"); 
+           return(inv); 
+      }
+       # getting to the data to compute
+       # the inverse 
+       data = x$get();
+       # computing the inverse 
+       i = solve(data, ...);
+       # storing the inverse in the object 
+       x$setInverse (i);
+
+       ## Return a matrix that is the inverse of 'x'	
+       i;
+        
 }
